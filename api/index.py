adde5f0895c243848f9f7ff4e1caf36985cb328b
alihamzababa73 <132482254+saifurrhman@users.noreply.github.com>
2026-01-31 12:53:56 +0500

    Structure

--- api/index.py
+++ api/index.py
@@ -1,14 +1,10 @@
 from http.server import BaseHTTPRequestHandler
-
+ 
 class handler(BaseHTTPRequestHandler):
+ 
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type','text/plain')
         self.end_headers()
-        self.wfile.write('Vercel Native Python is WORKING! 🚀'.encode('utf-8'))
+        self.wfile.write('Hello, World! Vercel Python Root Config Working!'.encode('utf-8'))
         return
-
-    # Catch-all for other methods
-    def do_POST(self): self.do_GET()
-    def do_PUT(self): self.do_GET()
-    def do_DELETE(self): self.do_GET()
