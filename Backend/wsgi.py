7d60826802296b007fb3b56467bced6c9d30d71f
alihamzababa73 <132482254+saifurrhman@users.noreply.github.com>
2026-01-31 12:38:38 +0500

    Fix: Add WSGI Error Trap

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import traceback
 
 path = os.path.dirname(os.path.abspath(__file__))
 if path not in sys.path:
@@ -7,7 +8,20 @@ if path not in sys.path:
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NextGenSmartStore.settings.production')
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
-
-app = application
+try:
+    from django.core.wsgi import get_wsgi_application
+    application = get_wsgi_application()
+    app = application
+except Exception:
+    # CATCH ALL STARTUP ERRORS and print to browser
+    error_trace = traceback.format_exc()
+    
+    def application(environ, start_response):
+        status = '200 OK'
+        output = f"CRITICAL STARTUP ERROR:\n\n{error_trace}".encode('utf-8')
+        response_headers = [('Content-type', 'text/plain'),
+                            ('Content-Length', str(len(output)))]
+        start_response(status, response_headers)
+        return [output]
+    
+    app = application
